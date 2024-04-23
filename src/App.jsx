a4b8171b80b7999706a2b851e4c8d2fc16da9961
Moustaquim Younes <moustaquimy@gmail.com>
2024-04-23 09:46:35 +0100

    Setting up the development environment.

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+function App() {
+
+  return (
+<>
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, dolore? Voluptatum reiciendis, quasi et, mollitia quibusdam quod dolorem impedit ea, similique repellendus temporibus perspiciatis eius reprehenderit ab qui odit error?
+
+</>
+);
+}
+
+
+export default App
