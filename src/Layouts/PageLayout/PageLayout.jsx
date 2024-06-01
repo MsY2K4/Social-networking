710ecb924143fc83ea748764d64f3784f86a194d
Moustaquim Younes <moustaquimy@gmail.com>
2024-06-01 10:38:35 +0100

    Update PageLayout.jsx

--- src/Layouts/PageLayout/PageLayout.jsx
+++ src/Layouts/PageLayout/PageLayout.jsx
@@ -1,25 +1,32 @@
+// Importing necessary components from Chakra UI and React Router
 import { Box, Flex } from "@chakra-ui/react";
 import Sidebar from "../../components/Sidebar/Sidebar";
 import { useLocation } from "react-router-dom";
 
-// instead of adding the Sidebar component to every page, we can add it only once to the PageLayout component and wrap the children with it. This way, we can have a sidebar on every page except the AuthPage.
-
+// Defining the PageLayout component
 const PageLayout = ({ children }) => {
-	const { pathname } = useLocation();
-	return (
-		<Flex>
-			{/* sidebar on the left */}
-			{pathname !== "/auth" ? (
-				<Box w={{ base: "70px", md: "240px" }}>
-					<Sidebar />
-				</Box>
-			) : null}
-			{/* the page content on the right */}
-			<Box flex={1} w={{ base: "calc(100% - 70px)", md: "calc(100% - 240px)" }}>
-				{children}
-			</Box>
-		</Flex>
-	);
+  // Extracting the current pathname from the location
+  const { pathname } = useLocation();
+  
+  return (
+    // Flex container for arranging content horizontally
+    <Flex>
+      {/* Left side (sidebar) */}
+      {pathname !== "/auth" ? (
+        // Rendering Sidebar component if the current path is not '/auth'
+        <Box w={{ base: "70px", md: "240px" }}>
+          <Sidebar />
+        </Box>
+      ) : null}
+      
+      {/* Right side (main content area) */}
+      <Box flex={1} w={{ base: "calc(100% - 70px)", md: "calc(100% - 240px)" }}>
+        {/* Rendering children components */}
+        {children}
+      </Box>
+    </Flex>
+  );
 };
 
+// Exporting the PageLayout component
 export default PageLayout;
