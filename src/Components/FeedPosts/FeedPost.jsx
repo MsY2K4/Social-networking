89e7494b1f329f4f688bf2cbf9b8f0b42a7da31f
Moustaquim Younes <moustaquimy@gmail.com>
2024-06-01 10:36:15 +0100

    Update FeedPost.jsx

--- src/Components/FeedPosts/FeedPost.jsx
+++ src/Components/FeedPosts/FeedPost.jsx
@@ -1,17 +1,25 @@
-import { Box, Image } from "@chakra-ui/react";
-import PostFooter from "./PostFooter";
-import PostHeader from "./PostHeader";
+import { Box, Image } from "@chakra-ui/react"; // Importing necessary components from Chakra UI library
+import PostFooter from "./PostFooter"; // Importing PostFooter component
+import PostHeader from "./PostHeader"; // Importing PostHeader component
+import useGetUserProfileById from "../../hooks/useGetUserProfileById"; // Custom hook for fetching user profile by ID
 
-const FeedPost = ({ img, username, avatar }) => {
-	return (
-		<>
-			<PostHeader username={username} avatar={avatar} />
-			<Box my={2} borderRadius={4} overflow={"hidden"}>
-				<Image src={img} alt={username} />
-			</Box>
-			<PostFooter username={username} />
-		</>
-	);
+// Define FeedPost component
+const FeedPost = ({ post }) => {
+    // Fetching user profile data based on the ID of the post creator
+    const { userProfile } = useGetUserProfileById(post.createdBy);
+
+    return (
+        <>
+            {/* Render post header */}
+            <PostHeader post={post} creatorProfile={userProfile} />
+            {/* Box to contain the post image */}
+            <Box my={2} borderRadius={4} overflow={"hidden"}>
+                <Image src={post.imageURL} alt={"FEED POST IMG"} />
+            </Box>
+            {/* Render post footer */}
+            <PostFooter post={post} creatorProfile={userProfile} />
+        </>
+    );
 };
 
-export default FeedPost;
+export default FeedPost; // Export FeedPost component
