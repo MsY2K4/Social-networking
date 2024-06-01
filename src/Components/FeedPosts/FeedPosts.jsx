92bf0629fcc8ba0c6d52501fee785558edd25d09
Moustaquim Younes <moustaquimy@gmail.com>
2024-06-01 10:36:18 +0100

    Update FeedPosts.jsx

--- src/Components/FeedPosts/FeedPosts.jsx
+++ src/Components/FeedPosts/FeedPosts.jsx
@@ -1,44 +1,45 @@
-import { Box, Container, Flex, Skeleton, SkeletonCircle, VStack } from "@chakra-ui/react";
-import FeedPost from "./FeedPost";
-import { useEffect, useState } from "react";
+import { Box, Container, Flex, Skeleton, SkeletonCircle, Text, VStack } from "@chakra-ui/react"; // Importing necessary components from Chakra UI library
+import FeedPost from "./FeedPost"; // Importing FeedPost component
+import useGetFeedPosts from "../../hooks/useGetFeedPosts"; // Custom hook for fetching feed posts
 
+// Define FeedPosts component
 const FeedPosts = () => {
-	const [isLoading, setIsLoading] = useState(true);
+    // Fetching feed posts and loading status using custom hook
+    const { isLoading, posts } = useGetFeedPosts();
 
-	useEffect(() => {
-		setTimeout(() => {
-			setIsLoading(false);
-		}, 2000);
-	}, []);
+    return (
+        <Container maxW={"container.sm"} py={10} px={2}>
+            {/* If loading, render skeleton placeholders */}
+            {isLoading &&
+                [0, 1, 2].map((_, idx) => (
+                    <VStack key={idx} gap={4} alignItems={"flex-start"} mb={10}>
+                        <Flex gap='2'>
+                            <SkeletonCircle size='10' />
+                            <VStack gap={2} alignItems={"flex-start"}>
+                                <Skeleton height='10px' w={"200px"} />
+                                <Skeleton height='10px' w={"200px"} />
+                            </VStack>
+                        </Flex>
+                        <Skeleton w={"full"}>
+                            <Box h={"400px"}>contents wrapped</Box>
+                        </Skeleton>
+                    </VStack>
+                ))}
 
-	return (
-		<Container maxW={"container.sm"} py={10} px={2}>
-			{isLoading &&
-				[0, 1, 2, 3].map((_, idx) => (
-					<VStack key={idx} gap={4} alignItems={"flex-start"} mb={10}>
-						<Flex gap='2'>
-							<SkeletonCircle size='10' />
-							<VStack gap={2} alignItems={"flex-start"}>
-								<Skeleton height='10px' w={"200px"} />
-								<Skeleton height='10px' w={"200px"} />
-							</VStack>
-						</Flex>
-						<Skeleton w={"full"}>
-							<Box h={"500px"}>contents wrapped</Box>
-						</Skeleton>
-					</VStack>
-				))}
+            {/* If not loading and there are posts, render each post */}
+            {!isLoading && posts.length > 0 && posts.map((post) => <FeedPost key={post.id} post={post} />)}
 
-			{!isLoading && (
-				<>
-					<FeedPost img='/img1.png' username='user_1' avatar='/img1.png' />
-					<FeedPost img='/img2.png' username='user_2' avatar='/img2.png' />
-					<FeedPost img='/img3.png' username='user_3' avatar='/img3.png' />
-					<FeedPost img='/img4.png' username='user_4' avatar='/img4.png' />
-				</>
-			)}
-		</Container>
-	);
+            {/* If not loading and there are no posts, display message */}
+            {!isLoading && posts.length === 0 && (
+                <>
+                    <Text fontSize={"md"} color={"red.400"}>
+                        No Posts Available.
+                    </Text>
+                    <Text color={"red.400"}>Add some friends!!</Text>
+                </>
+            )}
+        </Container>
+    );
 };
 
-export default FeedPosts;
+export default FeedPosts; // Export FeedPosts component
