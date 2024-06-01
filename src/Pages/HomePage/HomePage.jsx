6724d94bb690ebaef366c434f2833438f7904578
Moustaquim Younes <moustaquimy@gmail.com>
2024-06-01 10:38:59 +0100

    Update HomePage.jsx

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -1,20 +1,30 @@
+// Importing necessary components from Chakra UI and your custom components
 import { Box, Container, Flex } from "@chakra-ui/react";
 import FeedPosts from "../../components/FeedPosts/FeedPosts";
 import SuggestedUsers from "../../components/SuggestedUsers/SuggestedUsers";
 
+// Defining the HomePage component
 const HomePage = () => {
-	return (
-		<Container maxW={"container.lg"}>
-			<Flex gap={20}>
-				<Box flex={2} py={10}>
-					<FeedPosts />
-				</Box>
-				<Box flex={3} mr={20} display={{ base: "none", lg: "block" }} maxW={"300px"}>
-					<SuggestedUsers />
-				</Box>
-			</Flex>
-		</Container>
-	);
+  return (
+    // Container component limiting the maximum width of content
+    <Container maxW={"container.lg"}>
+      {/* Flex container for arranging content horizontally */}
+      <Flex gap={20}>
+        {/* Left section */}
+        <Box flex={2} py={10}>
+          {/* Rendering the FeedPosts component */}
+          <FeedPosts />
+        </Box>
+        
+        {/* Right section (displayed only on large screens) */}
+        <Box flex={3} mr={20} display={{ base: "none", lg: "block" }} maxW={"300px"}>
+          {/* Rendering the SuggestedUsers component */}
+          <SuggestedUsers />
+        </Box>
+      </Flex>
+    </Container>
+  );
 };
 
+// Exporting the HomePage component
 export default HomePage;
