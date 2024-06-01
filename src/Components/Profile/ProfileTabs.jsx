9754de63d9a94552c696d1367f405d7838c58fe1
Moustaquim Younes <moustaquimy@gmail.com>
2024-06-01 10:36:49 +0100

    Update ProfileTabs.jsx

--- src/Components/Profile/ProfileTabs.jsx
+++ src/Components/Profile/ProfileTabs.jsx
@@ -1,24 +1,21 @@
 import { Box, Flex, Text } from "@chakra-ui/react";
-import { BsBookmark, BsGrid3X3, BsSuitHeart } from "react-icons/bs";
+import { FaImage } from "react-icons/fa6";
 
 const ProfileTabs = () => {
 	return (
-		<Flex
-			w={"full"}
-			justifyContent={"center"}
-			gap={{ base: 4, sm: 10 }}
-			textTransform={"uppercase"}
-			fontWeight={"bold"}
-		>
-			<Flex borderTop={"1px solid white"} alignItems={"center"} p='3' gap={1} cursor={"pointer"}>
+		<Flex w={"full"}justifyContent={"center"}gap={{ base: 4, sm: 10 }}textTransform={"uppercase"}fontWeight={"bold"}>
+			{/* Tab for Posts */}
+			<Flex borderTop={"1px solid green"} alignItems={"center"} p='3' gap={1} cursor={"pointer"}>
 				<Box fontSize={20}>
-					<BsGrid3X3 />
+					<FaImage />
 				</Box>
 				<Text fontSize={12} display={{ base: "none", sm: "block" }}>
 					Posts
 				</Text>
 			</Flex>
-		</Flex>
+
+
+			</Flex>
 	);
 };
 
